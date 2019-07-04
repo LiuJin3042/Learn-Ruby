de6b6ca4a470b319d680e35f9f92813df283a659
Liu Jin <41297328+LiuJin3042@users.noreply.github.com>
2019-07-04 22:12:21 +0800

    update

--- gui_tk/ruby_calc.rb
+++ gui_tk/ruby_calc.rb
@@ -28,17 +28,6 @@ $tmp_input = ''
 # store histories
 $history = []
 
-# update tmp input and commands
-def update(input_num)
-  $commands += input_num
-  $tmp_input += input_num
-end
-
-# reset tmp input
-def reset
-  $tmp_input = ''
-end
-
 # commands label
 cmd_label = TkLabel.new(root){
   text $commands
@@ -48,23 +37,72 @@ cmd_label = TkLabel.new(root){
 # tmp input label
 tmp_label = TkLabel.new(root){
   text $tmp_input
+  pack :side=>'top', :fill=>'both'
 }
 
-def update(cmd_label, tmp_label, input_num)
-  $commands += input_num
-  $tmp_input += input_num
-  # it took me great pain to discover this method to
-  # change the label dynamically
-  cmd_label.configure('text'=>$commands)
-  tmp_label.configure('text'=>$tmp_input)
-  return NIL
-end
+# update label by eval(update)
+# it took me great pain to discover this method to
+# change the label dynamically
+# use '.configure' to update label
+# the method 'command' doesn't support function with parameters
+# so have to write the parameter to string and execute it
+update = "
+$commands += input_num
+$tmp_input += input_num
+cmd_label.configure('text'=>$commands)
+tmp_label.configure('text'=>$tmp_input)
+"
+# reset the tmp input label
+reset = "
+$commands += input_sym
+$tmp_input = ''
+cmd_label.configure('text'=>$commands)
+tmp_label.configure('text'=>$tmp_input)
+"
+
+# valuate commandso
+valuate = "
+
+"
+
 TkButton.new(root){
   input_num = '1'
   text input_num
   pack :side=>'top', :fill=>'both'
   command{
-    update(cmd_label, tmp_label, input_num)
- }
+    eval(update)
+  }
 }
+
+TkButton.new(root) do
+  input_sym = '+'
+  text input_sym
+  pack :side=>'top', :fill=>'both'
+  command{
+    eval(reset)
+  }
+end
+
+TkButton.new(root) do
+  get_ans = '='
+  text get_ans
+  pack :side=>'top', :fill=>'both'
+  command{
+    begin
+      ans = eval($commands).to_s
+      $commands = ''
+      $tmp_input = ''
+      # clear cmd label while saving the commands value to be
+      # called next time
+      cmd_label.configure('text'=>'')
+      tmp_label.configure('text'=>ans)
+    rescue => ex
+      $commands = ''
+      $tmp_input = ''
+      msg = ex.message
+      cmd_label.configure('text'=>msg)
+      tmp_label.configure('text'=>msg)
+    end
+    }
+end
 Tk.mainloop
